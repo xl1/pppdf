ad83bb5330a3bfcf41b73bb55894086ce9ad0418
xl1 <shiba200712@gmail.com>
2018-01-02 21:04:00 +0900

    Implement /pdf?html={html}

--- index.js
+++ index.js
@@ -4,18 +4,38 @@ const express = require('express');
 const app = express();
 const server = app.listen(8080, () => console.log('listening'));
 
-app.get('/pdf', async (req, res) => {
+function renderPage(page, query) {
+  if (query.url) {
+    return page.goto(query.url, {
+      timeout: 10 * 1000, // ms
+      waitUntil: 'networkidle0'
+    });
+  } else if (query.html) {
+    const dataUri = 'data:text/html;charset=utf-8,' + query.html;
+    return page.goto(dataUri, {
+      timeout: 10 * 1000, // ms
+      waitUntil: 'networkidle0'
+    });
+  }
+}
+
+async function ppp(func) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(req.query.url, {
-    timeout: 10 * 1000, // ms
-    waitUntil: 'networkidle2'
+  try {
+    await func(page);
+  } finally {
+    browser.close();
+  }
+}
+
+app.get('/pdf', (req, res) => {
+  ppp(async page => {
+    await renderPage(page, req.query);
+    res.writeHead(200, {
+      'Content-Type': 'application/pdf'
+    });
+    res.end(await page.pdf());
   });
-  const buffer = await page.pdf();
-  browser.close();
-  res.writeHead(200, {
-    'Content-Type': 'application/pdf'
-  });
-  res.end(buffer);
 });
 
